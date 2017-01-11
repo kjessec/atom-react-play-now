e5725f63c20078aa05b0eaa472a8c29dbac3c48c
Jesse Chung <kjesse.chung@samsung.com>
2017-01-12 03:42:55 +0900

    initial commit

---
+++ spec/react-play-now-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ReactPlayNowView from '../lib/react-play-now-view';
+
+describe('ReactPlayNowView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
